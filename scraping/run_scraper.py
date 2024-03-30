04002b547cd09e8d6f7bd1ff84649bc447217b9a
MerlinSchaefer <ms101196@googlemail.com>
2024-03-30 12:54:17 +0100

    Fix date usage in scraping url

--- scraping/run_scraper.py
+++ scraping/run_scraper.py
@@ -22,13 +22,16 @@ if __name__ == "__main__":
                 start_url_2days = json_dict.get("start_url_2days")
                 start_url_7days = json_dict.get("start_url_7days")
         except FileNotFoundError:
-            print(f"Error: '{json_file_path}' does not exist.")
-            exit()
+            raise Exception(f"Error: '{json_file_path}' does not exist.")
 
     if not start_url_2days or not start_url_7days:
-        print("Failed to obtain URLs. Exiting script.")
-        exit()
+        raise Exception("Failed to obtain URLs. Exiting script.")
     else:
+        # Replace {today} in the URLs with the current date
+        # Get the current date in DD.MM.YYYY format
+        today_date = datetime.now().strftime("%d.%m.%Y")
+        start_url_2days = start_url_2days.replace("{today}", today_date)
+        start_url_7days = start_url_7days.replace("{today}", today_date)
         data = scrape_bird_data(start_url_7days)
 
         # Save data as parquet
