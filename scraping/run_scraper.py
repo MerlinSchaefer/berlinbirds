35a256b1e05d3c86897af832e5920884200d1448
MerlinSchaefer <ms101196@googlemail.com>
2024-03-30 12:37:09 +0100

    Update actions workflow with env

--- scraping/run_scraper.py
+++ scraping/run_scraper.py
@@ -5,29 +5,33 @@ from datetime import datetime
 import pandas as pd
 from scraper import scrape_bird_data
 
-# TODO: embed into orchestration tool
-
 if __name__ == "__main__":
-    # Example usage
-    # Get the directory of the current script
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Try to read URLs from environment variables
+    start_url_2days = os.getenv("START_URL_2DAYS")
+    start_url_7days = os.getenv("START_URL_7DAYS")
 
-    # Construct the path to 'urls.json' relative to the script's directory
-    json_file_path = os.path.join(script_dir, "urls.json")
+    # If environment variables are not set, load from urls.json
+    if not start_url_2days or not start_url_7days:
+        print("Environment variables not found, attempting to load from urls.json...")
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        json_file_path = os.path.join(script_dir, "urls.json")
 
-    try:
-        with open(json_file_path, "r") as json_file:
-            json_dict = json.load(json_file)
-    except FileNotFoundError:
-        print(f"Error: '{json_file_path}' does not exist.")
+        try:
+            with open(json_file_path, "r") as json_file:
+                json_dict = json.load(json_file)
+                start_url_2days = json_dict.get("start_url_2days")
+                start_url_7days = json_dict.get("start_url_7days")
+        except FileNotFoundError:
+            print(f"Error: '{json_file_path}' does not exist.")
+            exit()
 
-    start_url_2days = json_dict.get("start_url_2days")
-    start_url_7days = json_dict.get("start_url_7days")
-    data = scrape_bird_data(start_url_7days)
+    if not start_url_2days or not start_url_7days:
+        print("Failed to obtain URLs. Exiting script.")
+        exit()
+    else:
+        data = scrape_bird_data(start_url_7days)
 
-    # save data as parquet
-    data_df = pd.DataFrame(data)
-    # Current time formatted in a way that is safe for filenames
-    now = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    # TODO: create logic to save at correct place
-    data_df.to_parquet(path=f"./data/{now}_past_7_day_data.parquet")
+        # Save data as parquet
+        data_df = pd.DataFrame(data)
+        now = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        data_df.to_parquet(path=f"./scraping_data/{now}_past_7_day_data.parquet")
